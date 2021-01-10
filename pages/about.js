89790fae037999b5a79b67124d792e32f3a9e2b4
@jpvalery <jpvalery@users.noreply.github.com>
2021-01-10 18:41:32 -0500

    feat: scaffolding for multiple pages

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+export default function Now() {
+  return (
+    <div>
+      <main>
+        <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
+          Who the heck is Jp
+        </h1>
+      </main>
+    </div>
+  )
+}
---
+++ pages/now.js
@@ -0,0 +1,11 @@
+export default function Now() {
+  return (
+    <div>
+      <main>
+        <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
+          What am I up to now?
+        </h1>
+      </main>
+    </div>
+  )
+}
