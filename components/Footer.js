24c87a7b3c521a3bb1234cd9c59e1caa5c146436
@jpvalery <jpvalery@users.noreply.github.com>
2021-01-10 16:52:13 -0500

    feat: Header & general layout

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,7 @@ import Unsplash from '../icons/Unsplash';
 export default function Footer () {
   return (
     <footer className="bg-white">
-      <div className="max-w-3xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
+      <div className="max-w-3xl mx-auto py-12 md:flex md:items-center md:justify-between">
         <div className="flex justify-center space-x-6 md:order-2">
           <a href="https://linkedin.com/in/jpvalery" className="text-gray-400 hover:text-gray-500 h-6 w-6">
             <LinkedIn />
