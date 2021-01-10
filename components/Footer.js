0cd59c13ab6712515bc54e446d633aa8d47b7c72
@jpvalery <jpvalery@users.noreply.github.com>
2021-01-10 14:58:31 -0500

    feat: footer

---
+++ components/Footer.js
@@ -0,0 +1,36 @@
+import GitHub from '../icons/Github';
+import LinkedIn from '../icons/Linkedin';
+import Twitter from '../icons/Twitter';
+import Unsplash from '../icons/Unsplash';
+
+export default function Footer () {
+  return (
+    <footer className="bg-white">
+      <div className="max-w-3xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
+        <div className="flex justify-center space-x-6 md:order-2">
+          <a href="https://linkedin.com/in/jpvalery" className="text-gray-400 hover:text-gray-500 h-6 w-6">
+            <LinkedIn />
+          </a>
+
+          <a href="https://unsplash.com/@jpvalery" className="text-gray-400 hover:text-gray-500 h-6 w-6">
+            <Unsplash />
+          </a>
+
+          <a href="https://github.com/jpvalery" className="text-gray-400 hover:text-gray-500 h-6 w-6">
+            <GitHub />
+          </a>
+
+          <a href="https://twitter.com/jpvalery" className="text-gray-400 hover:text-gray-500 h-6 w-6">
+            <Twitter />
+          </a>
+
+        </div>
+        <div className="mt-8 md:mt-0 md:order-1">
+          <p className="text-center text-base text-gray-400">
+            &copy; 1992-2021 Jp Valery. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  )
+}
