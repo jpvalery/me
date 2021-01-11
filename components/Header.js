42e1ff6b931a156da02fe764102b8b87a2ba237c
@jpvalery <jpvalery@users.noreply.github.com>
2021-01-11 17:40:57 -0500

    feat: external icon + target _blank for external links

--- components/Header.js
+++ components/Header.js
@@ -50,15 +50,19 @@ export default function Header () {
                         <div className="relative grid gap-8 bg-white px-8 py-8">
                           <Menu.Item>
                             {({ active }) => (
-                              <a href="https://jpvalery.photo" className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
+                              <a href="https://jpvalery.photo" target="_blank" className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
 
                                 <svg className="flex-shrink-0 h-6 w-6 text-indigo-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z" />
                                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 13a3 3 0 11-6 0 3 3 0 016 0z" />
                                 </svg>
                                 <div className="ml-4">
-                                  <p className="text-base font-medium text-gray-900">
+                                  <p className="text-base font-medium text-gray-900 inline-flex">
                                     Portfolio
+                                    <svg className="mt-1 ml-2 h-4 w-4 text-gray-700" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                      <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
+                                      <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
+                                    </svg>
                                   </p>
                                   <p className="mt-1 text-sm text-gray-500">
                                     My photography portfolio
@@ -70,14 +74,18 @@ export default function Header () {
 
                           <Menu.Item>
                             {({ active }) => (
-                              <a href="https://archive.jpvalery.photo" className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
+                              <a href="https://archive.jpvalery.photo" target="_blank" className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
 
                                 <svg className="flex-shrink-0 h-6 w-6 text-indigo-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 5c7.18 0 13 5.82 13 13M6 11a7 7 0 017 7m-6 0a1 1 0 11-2 0 1 1 0 012 0z" />
                                 </svg>
                                 <div className="ml-4">
-                                  <p className="text-base font-medium text-gray-900">
+                                  <p className="text-base font-medium text-gray-900 inline-flex">
                                     Photo Archive
+                                    <svg className="mt-1 ml-2 h-4 w-4 text-gray-700" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                      <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
+                                      <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
+                                    </svg>
                                   </p>
                                   <p className="mt-1 text-sm text-gray-500">
                                     An unfiltered stream of all my photos
@@ -89,14 +97,18 @@ export default function Header () {
 
                           <Menu.Item>
                             {({ active }) => (
-                              <a href="https://montrealphoto.club" className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
+                              <a href="https://montrealphoto.club" target="_blank" className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
 
                                 <svg className="flex-shrink-0 h-6 w-6 text-indigo-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
                                 </svg>
                                 <div className="ml-4">
-                                  <p className="text-base font-medium text-gray-900">
+                                  <p className="text-base font-medium text-gray-900 inline-flex">
                                     Montréal Photo Club
+                                    <svg className="mt-1 ml-2 h-4 w-4 text-gray-700" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                      <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
+                                      <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
+                                    </svg>
                                   </p>
                                   <p className="mt-1 text-sm text-gray-500">
                                     I founded a Photo Club in Montréal in 2019
@@ -108,14 +120,18 @@ export default function Header () {
 
                           <Menu.Item>
                             {({ active }) => (
-                              <a href="https://unsplash.com/@jpvalery" className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
+                              <a href="https://unsplash.com/@jpvalery" target="_blank" className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
 
                                 <svg className="flex-shrink-0 h-6 w-6 text-indigo-800" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                   <path d="M7.5 6.75V0h9v6.75h-9zm9 3.75H24V24H0V10.5h7.5v6.75h9V10.5z" />
                                 </svg>
                                 <div className="ml-4">
-                                  <p className="text-base font-medium text-gray-900">
+                                  <p className="text-base font-medium text-gray-900 inline-flex">
                                     Unsplash
+                                    <svg className="mt-1 ml-2 h-4 w-4 text-gray-700" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                      <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
+                                      <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
+                                    </svg>
                                   </p>
                                   <p className="mt-1 text-sm text-gray-500">
                                     I share hundreds of photos for free on Unsplash
@@ -336,23 +352,31 @@ export default function Header () {
 
                 <div className="mt-6">
                   <nav className="grid gap-y-8">
-                  <a href="https://jpvalery.photo" className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
+                  <a href="https://jpvalery.photo" target="_blank"  className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
 
                     <svg className="flex-shrink-0 h-6 w-6 text-indigo-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z" />
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 13a3 3 0 11-6 0 3 3 0 016 0z" />
                     </svg>
-                    <span className="ml-3 text-base font-medium text-gray-900">
+                    <span className="ml-3 text-base font-medium text-gray-900 inline-flex">
                       Portfolio
+                      <svg className="mt-1 ml-2 h-4 w-4 text-gray-700" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                        <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
+                        <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
+                      </svg>
                     </span>
                   </a>
 
-                    <a href="https://archive.jpvalery.photo" className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
+                    <a href="https://archive.jpvalery.photo" target="_blank" className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
                     <svg className="flex-shrink-0 h-6 w-6 text-indigo-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 5c7.18 0 13 5.82 13 13M6 11a7 7 0 017 7m-6 0a1 1 0 11-2 0 1 1 0 012 0z" />
                     </svg>
-                      <span className="ml-3 text-base font-medium text-gray-900">
+                      <span className="ml-3 text-base font-medium text-gray-900 inline-flex">
                         Photo Archive
+                        <svg className="mt-1 ml-2 h-4 w-4 text-gray-700" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                          <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
+                          <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
+                        </svg>
                       </span>
                     </a>
 
