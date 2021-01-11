6773844521c7072c65c14ab20cdc0d827e66d488
@jpvalery <jpvalery@users.noreply.github.com>
2021-01-11 14:08:11 -0500

    feat: mobile menu

--- components/Header.js
+++ components/Header.js
@@ -336,62 +336,83 @@ export default function Header () {
 
                 <div className="mt-6">
                   <nav className="grid gap-y-8">
-                    <a href="#" className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
-                      <svg className="flex-shrink-0 h-6 w-6 text-indigo-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
-                      </svg>
+                  <a href="https://jpvalery.photo" className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
+
+                    <svg className="flex-shrink-0 h-6 w-6 text-indigo-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z" />
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 13a3 3 0 11-6 0 3 3 0 016 0z" />
+                    </svg>
+                    <span className="ml-3 text-base font-medium text-gray-900">
+                      Portfolio
+                    </span>
+                  </a>
+
+                    <a href="https://archive.jpvalery.photo" className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
+                    <svg className="flex-shrink-0 h-6 w-6 text-indigo-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 5c7.18 0 13 5.82 13 13M6 11a7 7 0 017 7m-6 0a1 1 0 11-2 0 1 1 0 012 0z" />
+                    </svg>
                       <span className="ml-3 text-base font-medium text-gray-900">
-                        Analytics
+                        Photo Archive
                       </span>
                     </a>
 
-                    <a href="#" className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
-                      <svg className="flex-shrink-0 h-6 w-6 text-indigo-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122" />
-                      </svg>
+                    <NextLink href="/about">
+                    <span className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
+                    <svg className="flex-shrink-0 h-6 w-6 text-indigo-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
                       <span className="ml-3 text-base font-medium text-gray-900">
-                        Engagement
+                        /about
                       </span>
-                    </a>
+                      </span>
+                    </NextLink>
 
-                    <a href="#" className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
-                      <svg className="flex-shrink-0 h-6 w-6 text-indigo-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
-                      </svg>
+                    <NextLink href="/now">
+                    <span className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
+                    <svg className="flex-shrink-0 h-6 w-6 text-indigo-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5.636 18.364a9 9 0 010-12.728m12.728 0a9 9 0 010 12.728m-9.9-2.829a5 5 0 010-7.07m7.072 0a5 5 0 010 7.07M13 12a1 1 0 11-2 0 1 1 0 012 0z" />
+                    </svg>
                       <span className="ml-3 text-base font-medium text-gray-900">
-                        Security
+                        /now
                       </span>
-                    </a>
+                    </span>
+                    </NextLink>
 
-                    <a href="#" className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
-                      <svg className="flex-shrink-0 h-6 w-6 text-indigo-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
-                      </svg>
+                    <NextLink href="/stack">
+                    <span href="#" className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
+                    <svg className="flex-shrink-0 h-6 w-6 text-indigo-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10" />
+                    </svg>
                       <span className="ml-3 text-base font-medium text-gray-900">
-                        Integrations
+                        /stack
                       </span>
-                    </a>
+                    </span>
+                    </NextLink>
 
-                    <a href="#" className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
-                      <svg className="flex-shrink-0 h-6 w-6 text-indigo-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
-                      </svg>
+                    <NextLink href="/dashboard">
+                    <span href="#" className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
+                    <svg className="flex-shrink-0 h-6 w-6 text-indigo-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 8v8m-4-5v5m-4-2v2m-2 4h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                    </svg>
                       <span className="ml-3 text-base font-medium text-gray-900">
-                        Automations
+                        /dashboard
                       </span>
-                    </a>
+                    </span>
+                    </NextLink>
+
+                    <NextLink href="/cemetery">
+                    <span href="#" className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
+                    <svg className="flex-shrink-0 h-6 w-6 text-indigo-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+                    </svg>
+                      <span className="ml-3 text-base font-medium text-gray-900">
+                        /cemetery
+                      </span>
+                    </span>
+                    </NextLink>
+
+
                   </nav>
-                </div>
-              </div>
-              <div className="py-6 px-5 space-y-6">
-                <div className="grid grid-cols-2 gap-y-4 gap-x-8">
-                  <a href="#" className="text-base font-medium text-gray-900 hover:text-gray-700">
-                    Pricing
-                  </a>
-
-                  <a href="#" className="text-base font-medium text-gray-900 hover:text-gray-700">
-                    Docs
-                  </a>
                 </div>
               </div>
             </div>
