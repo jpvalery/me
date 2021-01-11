3e1a1938d02e735e9b9cbee72fd431726fe4df0c
@jpvalery <jpvalery@users.noreply.github.com>
2021-01-10 22:53:50 -0500

    feat: content for main pages

--- pages/now.js
+++ pages/now.js
@@ -5,6 +5,18 @@ export default function Now() {
         <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
           What am I up to now?
         </h1>
+        <p class="text-base font-semibold mt-4 tracking-wide uppercase">Last updated: January 10th, 2021</p>
+        <article class="prose prose-xl mt-4">
+        <p>Well... 2020 was a effing dumpster fire, wasn't it?</p>
+        <p>I'm hoping 2021 will be better (<em>as everyone probably is</em>) but let's say that I'm cautiously optimistic. Let's see how things are after our latest lockdown.</p>
+        <p>On the bright side, I think the dumpster fire allowed me to really figure out what was important for me and what I wanted from my life.</p>
+        <p>We also had a great harvest in our veggie garden over the summer and I converted my landlord and neighbor to the joy of growing your own food.</p>
+        <p>I also built a flight sim chair out of wood and a recycled auto seat, as well as a custom joystick with an Arduino and some soldered buttons.</p>
+        <p>These days I'm playing mostly Apex Legends and Rainbow Six Siege. The latest Red Dead Online update was a bloody joke and literally made me churn from the game. I've also started <a href="https://lichess.org/@/jpvalery">playing chess online</a> and I'm neither great nor terrible.</p>
+        <p>I've been reading a lot of books on self-sufficiency and homesteading. I've also started preparing for the next growing seasons and can't wait to receive my order of seeds.</p>
+        <p>Oh and, you've guessed it, I'm cleaning up my websites and domains. Kinda like spring cleaning but with DNS records and code and it's winter.</p>
+        <p><em>Also wondering if I should save these updates as some sort of seasonal journal.</em></p>
+        </article>
       </main>
     </div>
   )
