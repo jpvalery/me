e95e6dd92becd19fe04b6a2e8f22f3416e319aaf
@jpvalery <jpvalery@users.noreply.github.com>
2021-01-10 22:53:50 -0500

    feat: content for main pages

--- pages/stack.js
+++ pages/stack.js
@@ -3,8 +3,56 @@ export default function Stack() {
     <div>
       <main>
         <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
-          What are my fav services/products?
+          What am I using?
         </h1>
+        <article class="prose mt-4">
+          <h3 id="everyday-tech">Everyday tech</h3>
+          <ul>
+          <li>iPhone SE 2020</li>
+          <li>Apple Watch Series 3</li>
+          <li>Airpods</li>
+          <li>Steelseries Arctis 3</li>
+          </ul>
+          <h3 id="everyday-tech">Software</h3>
+          <ul>
+          <li>Firefox (diehard)</li>
+          <li>1Password</li>
+          <li>Backblaze</li>
+          <li>Atom</li>
+          <li>Starship (terminal)</li>
+          </ul>
+          <h3 id="photography">Photography</h3>
+          <ul>
+          <li>Canon EOS 6D</li>
+          <li>Olympus OMD-M10-MkII</li>
+          <li>Olympus E-PL1 (infrared)</li>
+          <li>Mamiya RB67 (film)</li>
+          <li>DJI Mavic Air 2</li>
+          <li>DJI Phantom 3 Pro</li>
+          </ul>
+          <h3 id="gaming">Gaming</h3>
+          <ul>
+          <li>Xbox Series X</li>
+          <li>Steelseries Arctis 3</li>
+          </ul>
+          <h3 id="office-work">Office - Work</h3>
+          <ul>
+          <li>MBP 13&quot; 2019</li>
+          <li>Dell 24&quot; monitor</li>
+          <li>Apple Keyboard 2014</li>
+          <li>Magic Mouse</li>
+          <li>XL Mouse Pad</li>
+          <li>Tresanti Standing Desk</li>
+          </ul>
+          <h3 id="office-personal">Office - Personal</h3>
+          <ul>
+          <li>iMac 27&quot; 2017</li>
+          <li>Asus 19&quot;</li>
+          <li>Lacie HDD</li>
+          <li>Magic Keyboard</li>
+          <li>Magic Trackpad</li>
+          </ul>
+        </article>
       </main>
     </div>
   )
