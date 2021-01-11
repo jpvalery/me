204b232b8645536f112f5b20119bb02ae211ccae
@jpvalery <jpvalery@users.noreply.github.com>
2021-01-11 13:56:39 -0500

    fix: misc

---
+++ pages/writings.js
@@ -0,0 +1,14 @@
+export default function Writings() {
+  return (
+    <div>
+      <main>
+        <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
+          Writings
+        </h1>
+        <article class="prose prose-xl mt-4">
+        <p>Hoping to populate this section soon</p>
+        </article>
+      </main>
+    </div>
+  )
+}
