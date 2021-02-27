e453f2daf196b1df518ba3c2babe9f8963c3ea0f
jpvalery <jpvalery@users.noreply.github.com>
2021-02-27 17:20:25 -0500

    feat: new contact + prettier

--- components/Homecard.js
+++ components/Homecard.js
@@ -79,9 +79,7 @@ export default function Homecard() {
               d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
             />
           </svg>
-          <span>
-            Growth Operations / Customer Success
-          </span>
+          <span>Growth Operations / Customer Success</span>
         </span>
 
         <span className="text-gray-900 flex items-center px-3 py-2 text-sm font-medium">
