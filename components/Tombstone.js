c5a76b589bc02ef232cc0f1ac434f851d19fd3a7
jpvalery <jpvalery@users.noreply.github.com>
2021-03-30 10:27:58 -0400

    feat: next@10.1 & @heroicons/react + updates

--- components/Tombstone.js
+++ components/Tombstone.js
@@ -1,43 +1,24 @@
+import { ArchiveIcon, ExternalLinkIcon } from "@heroicons/react/outline";
+
 export default function Tombstone({ title, years, link, description }) {
   return (
-    <div className="overflow-hidden rounded-lg shadow divide-y divide-gray-200">
+    <div className="overflow-hidden divide-y divide-gray-200 rounded-lg shadow">
       <div className="px-4 py-5 bg-gray-500 sm:px-6">
         <div className="flex items-center justify-between">
           <div className="flex items-center justify-between text-gray-900">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              className="w-6 h-6 mr-2"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"
-              />
-            </svg>
+            <ArchiveIcon className="w-6 h-6 mr-2" />
             <h2 className="text-xl font-bold leading-6">{title}</h2>
-            <span className="pt-1 ml-2 text-xs font-medium text-gray-800 leading-6">
+            <span className="pt-1 ml-2 text-xs font-medium leading-6 text-gray-800">
               ({years || "-"})
             </span>
           </div>
           <div>
             <a
               href={link || "#"}
-              className="relative inline-flex items-center px-4 py-2 ml-3 text-sm font-medium text-gray-700 bg-white border border-gray-300 shadow-sm rounded-md hover:bg-gray-50 focus:outline-none"
+              className="relative flex items-center justify-between px-4 py-2 ml-3 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none"
             >
               <span>Visit</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                className="w-5 h-5 ml-2"
-              >
-                <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
-                <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
-              </svg>
+              <ExternalLinkIcon className="w-4 h-4 ml-2" />
             </a>
           </div>
         </div>
--- pages/stack.js
+++ pages/stack.js
@@ -1,3 +1,12 @@
+import {
+  DeviceMobileIcon,
+  ChipIcon,
+  CameraIcon,
+  CubeTransparentIcon,
+  BriefcaseIcon,
+  DesktopComputerIcon,
+} from "@heroicons/react/outline";
+
 export default function Stack() {
   return (
     <main>
@@ -11,20 +20,7 @@ export default function Stack() {
       <container className="grid gap-8 mt-4 grid-cols1 sm:grid-cols-2">
         <div className="p-5 overflow-hidden bg-gray-800 rounded-lg shadow">
           <h3 className="flex items-center text-lg font-semibold text-white">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              className="w-6 h-6 mr-2"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2 2v14a2 2 0 002 2z"
-              />
-            </svg>
+            <DeviceMobileIcon className="w-6 h-6 mr-2" />
             Everyday tech
           </h3>
           <div className="mt-4">
@@ -41,20 +37,7 @@ export default function Stack() {
 
         <div className="p-5 overflow-hidden bg-gray-800 rounded-lg shadow metric-card">
           <h3 className="flex items-center text-lg font-semibold text-white">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              className="w-6 h-6 mr-2"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z"
-              />
-            </svg>
+            <ChipIcon className="w-6 h-6 mr-2" />
             Software
           </h3>
           <div className="mt-4">
@@ -63,7 +46,7 @@ export default function Stack() {
                 <li>Firefox (diehard)</li>
                 <li>1Password</li>
                 <li>Backblaze</li>
-                <li>Atom</li>
+                <li>VSCode</li>
                 <li>Starship (terminal)</li>
               </ul>
             </span>
@@ -72,26 +55,7 @@ export default function Stack() {
 
         <div className="p-5 overflow-hidden bg-gray-800 rounded-lg shadow metric-card">
           <h3 className="flex items-center text-lg font-semibold text-white">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              className="w-6 h-6 mr-2"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"
-              />
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"
-              />
-            </svg>
+            <CameraIcon className="w-6 h-6 mr-2" />
             Photography
           </h3>
           <div className="mt-4">
@@ -110,20 +74,7 @@ export default function Stack() {
 
         <div className="p-5 overflow-hidden bg-gray-800 rounded-lg shadow metric-card">
           <h3 className="flex items-center text-lg font-semibold text-white">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              className="w-6 h-6 mr-2"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M14 10l-2 1m0 0l-2-1m2 1v2.5M20 7l-2 1m2-1l-2-1m2 1v2.5M14 4l-2-1-2 1M4 7l2-1M4 7l2 1M4 7v2.5M12 21l-2-1m2 1l2-1m-2 1v-2.5M6 18l-2-1v-2.5M18 18l2-1v-2.5"
-              />
-            </svg>
+            <CubeTransparentIcon className="w-6 h-6 mr-2" />
             Gaming
           </h3>
           <div className="mt-4">
@@ -141,27 +92,14 @@ export default function Stack() {
 
         <div className="p-5 overflow-hidden bg-gray-800 rounded-lg shadow metric-card">
           <h3 className="flex items-center text-lg font-semibold text-white">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              className="w-6 h-6 mr-2"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-              />
-            </svg>
+            <BriefcaseIcon className="w-6 h-6 mr-2" />
             Office - Work
           </h3>
           <div className="mt-4">
             <span className="prose text-gray-300">
               <ul>
-                <li>MBP 13&quot; 2019</li>
-                <li>Dell 24&quot; monitor</li>
+                <li>MBP M1 13&quot; 2020</li>
+                <li>HP Z27 monitor</li>
                 <li>Apple Keyboard 2014</li>
                 <li>Magic Mouse</li>
                 <li>XL Mouse Pad</li>
@@ -173,20 +111,7 @@ export default function Stack() {
 
         <div className="p-5 overflow-hidden bg-gray-800 rounded-lg shadow metric-card">
           <h3 className="flex items-center text-lg font-semibold text-white">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              className="w-6 h-6 mr-2"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-              />
-            </svg>
+            <DesktopComputerIcon className="w-6 h-6 mr-2" />
             Office - Personal
           </h3>
           <div className="mt-4">
